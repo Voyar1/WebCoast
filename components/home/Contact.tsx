af701f3903064cfc8e0bbfa85067f47a155a8c55
Arkadiusz <areq12x@gmail.com>
2024-03-16 11:07:58 +0100

    Implemented funcionality to recieve messages from form.

--- components/home/Contact.tsx
+++ components/home/Contact.tsx
@@ -1,22 +1,8 @@
 "use client";
-import { useForm, SubmitHandler } from "react-hook-form";
 
-type Inputs = {
-  example: string;
-  exampleRequired: string;
-};
+import ContactForm from "../shared/ContactForm";
 
 export default function App() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
-
-  console.log(watch("example")); // watch input value by passing the name of it
-
   return (
     <section className="z-50 m-auto mt-12  w-8/12 max-sm:w-full max-sm:px-4 xl:w-6/12">
       <h1 className="h1-bold text-center">Skontaktuj się z nami!</h1>
@@ -31,50 +17,7 @@ export default function App() {
             Send a message!
           </a>
         </article>
-
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="m-auto flex w-full flex-col gap-4"
-        >
-          {/* register your input into the hook by invoking the "register" function */}
-          <input
-            placeholder="Imię"
-            {...register("example")}
-            className="rounded-xl bg-light-400 p-2 pl-4 text-black"
-          />
-
-          <input
-            placeholder="Nazwisko"
-            {...register("example")}
-            className="rounded-xl bg-light-400 p-2 pl-4"
-          />
-
-          {/* include validation with required or other standard HTML validation rules */}
-          <input
-            {...register("exampleRequired", { required: true })}
-            className="rounded-xl bg-light-400 p-2 pl-4"
-            placeholder="E-mail"
-          />
-          {/* errors will return when field validation fails  */}
-          {errors.exampleRequired && <span>This field is required</span>}
-          <input
-            {...register("exampleRequired", { required: true })}
-            className="rounded-xl bg-light-400 p-2 pl-4"
-            placeholder="Numer telefonu"
-          />
-          <textarea
-            name="message"
-            id=""
-            cols={30}
-            rows={6}
-            placeholder="Your Message"
-            className="rounded-xl bg-light-400 p-2 pl-4"
-          ></textarea>
-          <input
-            type="submit"
-            className="primary-gradient mb-20 rounded-xl py-2 text-xl font-bold text-white"
-          />
-        </form>
+        <ContactForm />
       </div>
     </section>
   );
---
+++ components/shared/ContactForm.tsx
@@ -0,0 +1,113 @@
+"use client";
+import React, { useRef } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import emailjs from "@emailjs/browser";
+
+type Inputs = {
+  firstName: string;
+  email: string;
+  phone: string;
+  message: string;
+};
+
+const ContactForm = () => {
+  const form = useRef();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({
+    defaultValues: {
+      firstName: "",
+      email: "",
+      phone: "",
+      message: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<Inputs> = async (data, e) => {
+    e?.preventDefault();
+
+    const templateParams = {
+      firstName: data.firstName,
+      email: data.email,
+      phone: data.phone,
+      message: data.message,
+    };
+
+    const formData = JSON.stringify(templateParams);
+
+    await emailjs
+      .sendForm("service_cl58jle", "template_7hgnnhb", "#contact_form", {
+        publicKey: "tqf0pZAjua2qbwfYA",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error);
+        },
+      );
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="m-auto mb-8 flex w-full flex-col gap-4"
+      id="contact_form"
+    >
+      <div className="flex flex-col">
+        <input
+          placeholder="Imię"
+          {...register("firstName", { required: true, maxLength: 20 })}
+          className="rounded-xl bg-light-400 p-2 pl-4 text-black"
+        />
+
+        {errors.firstName && (
+          <span className="text-red-300">To pole jest wymagane</span>
+        )}
+      </div>
+      <div className="flex flex-col">
+        <input
+          {...register("email", { required: true })}
+          className="rounded-xl bg-light-400 p-2 pl-4"
+          placeholder="E-mail"
+          type="email"
+        />
+        {errors.email && (
+          <span className="text-red-300">To pole jest wymagane</span>
+        )}
+      </div>
+      <div className="flex flex-col">
+        <input
+          {...register("phone")}
+          className="rounded-xl bg-light-400 p-2 pl-4"
+          placeholder="Numer telefonu"
+          type="tel"
+        />
+      </div>
+      <div className="flex flex-col">
+        <textarea
+          {...register("message", { required: true })}
+          name="message"
+          id="message"
+          cols={30}
+          rows={6}
+          placeholder="Your Message"
+          className="rounded-xl bg-light-400 p-2 pl-4"
+        ></textarea>
+        {errors.message && (
+          <span className="text-red-300">To pole jest wymagane</span>
+        )}
+      </div>
+
+      <input
+        type="submit"
+        className="cursor-pointer rounded-xl  bg-primary-500 py-2 text-xl font-bold text-white shadow-lg transition-all duration-300 ease-in-out hover:bg-primary-300"
+      />
+    </form>
+  );
+};
+
+export default ContactForm;
